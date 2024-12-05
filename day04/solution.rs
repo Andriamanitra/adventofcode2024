6fcf554eec0657c78ee1df4e2fe449b72b84150f
Andriamanitra <mikko77792@gmail.com>
2024-12-05 05:01:43 +0200

    human > rustfmt

--- day04/solution.rs
+++ day04/solution.rs
@@ -1,31 +1,18 @@
 fn part1(grid: &[Vec<char>]) -> usize {
-    const DIRECTIONS: [(i32, i32); 8] = [
-        (1, 0),
-        (1, 1),
-        (0, 1),
-        (-1, 1),
-        (-1, 0),
-        (-1, -1),
-        (0, -1),
-        (1, -1),
-    ];
+    const DIRECTIONS: [(i32, i32); 8] = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)];
     let height = grid.len() as i32;
     let width = grid[0].len() as i32;
     let mut count = 0;
     for r in 0..height {
         for c in 0..width {
             for (dr, dc) in DIRECTIONS {
-                count += {
-                    "XMAS".chars().enumerate().all(|(i, letter)| {
-                        let r = r + i as i32 * dr;
-                        let c = c + i as i32 * dc;
-                        0 <= r
-                            && r < height
-                            && 0 <= c
-                            && c < width
-                            && grid[r as usize][c as usize] == letter
-                    }) as usize
-                };
+                count += "XMAS".chars().enumerate().all(|(i, letter)| {
+                    let r = r + i as i32 * dr;
+                    let c = c + i as i32 * dc;
+                    0 <= r && r < height
+                    && 0 <= c && c < width
+                    && grid[r as usize][c as usize] == letter
+                }) as usize;
             }
         }
     }
@@ -33,19 +20,23 @@ fn part1(grid: &[Vec<char>]) -> usize {
 }
 
 fn part2(grid: &[Vec<char>]) -> usize {
-    const DIAGONALS: [[(i32, i32); 3]; 2] =
-        [[(-1, -1), (0, 0), (1, 1)], [(1, -1), (0, 0), (-1, 1)]];
+    const DIAGONALS: [[(i32, i32); 3]; 2] =[
+        [(-1, -1), (0, 0), (1, 1)],
+        [(1, -1), (0, 0), (-1, 1)],
+    ];
     let mut count = 0;
     let height = grid.len() as i32;
     let width = grid.len() as i32;
     for r in 1..height - 1 {
         for c in 1..width - 1 {
-            count += (grid[r as usize][c as usize] == 'A'
+            count += (
+                grid[r as usize][c as usize] == 'A'
                 && DIAGONALS.iter().all(|[(dr1, dc1), _, (dr2, dc2)]| {
                     let (r1, c1) = ((r + dr1) as usize, (c + dc1) as usize);
                     let (r2, c2) = ((r + dr2) as usize, (c + dc2) as usize);
                     matches!((grid[r1][c1], grid[r2][c2]), ('M', 'S') | ('S', 'M'))
-                })) as usize;
+                })
+            ) as usize;
         }
     }
     count
