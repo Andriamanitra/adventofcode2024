a1dfc43c705f998fcb9173edda483672342da1d8
Andriamanitra <mikko77792@gmail.com>
2024-12-06 09:24:30 +0200

    improve day 6 ruby solution

--- day06/solution.rb
+++ day06/solution.rb
@@ -1,51 +1,55 @@
-ARGV << 'input.txt'
 input = ARGF.read
-input = input.lines(chomp: true).map(&:chars)
+input = input.lines(chomp: true).map(&:bytes)
 
-$h = input.size
-$w = input[0].size
-coords = [*0...$h].product([*0...$w])
-$gi, $gj = coords.find{|i,j|input[i][j] == ?^}
+H = input.size
+W = input[0].size
+GUARD = '^'.ord
+OBSTACLE = '#'.ord
+EMPTY = '.'.ord
 
-$dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+def find_guard(input)
+  (0...H).each do |i|
+    (0...W).each do |j|
+      return [i,j] if input[i][j] == GUARD
+    end
+  end
+end
 
 def walk(input)
+  i, j = find_guard(input)
+  di, dj = -1, 0
   part2 = 0
-  i, j = $gi, $gj
-  d = 0
   visited = Set[]
-  while 0 <= i && i < $h && 0 <= j && j < $w
-    if visited.add?([i, j])
-      input[i][j] = '#'
-      part2 += 1 if loop?(input, i - $dirs[d][0], j - $dirs[d][1], (d+1)%4)
-      input[i][j] = '.'
+  while 0 <= i && i < H && 0 <= j && j < W
+    if visited.add?((i << 16) | j)
+      input[i][j] = OBSTACLE
+      part2 += 1 if loop?(input, i - di, j - dj, di, dj)
+      input[i][j] = EMPTY
     end
 
-    ni = i + $dirs[d][0]
-    nj = j + $dirs[d][1]
-    if ni >= 0 && nj >= 0 && input.dig(ni, nj) == '#'
-      d += 1
-      d = 0 if d == 4
-    else
-      i = ni
-      j = nj
+    i += di
+    j += dj
+    break if i < 0 || j < 0 || i >= H || j >= W
+    if input[i][j] == OBSTACLE
+      i -= di
+      j -= dj
+      di, dj = dj, -di
     end
   end
   [visited.size, part2]
 end
 
-def loop?(input, i, j, d)
+def loop?(input, i, j, di, dj)
   visited = Set[]
-  while 0 <= i && i < $h && 0 <= j && j < $w
-    return true if !visited.add?([i, j, d])
-    ni = i + $dirs[d][0]
-    nj = j + $dirs[d][1]
-    if ni >= 0 && nj >= 0 && input.dig(ni, nj) == '#'
-      d += 1
-      d = 0 if d == 4
-    else
-      i = ni
-      j = nj
+  while 0 <= i && i < H && 0 <= j && j < W
+    return true if !visited.add?((i << 18) | (j << 4) | (di+1<<2) | (dj+1))
+    i += di
+    j += dj
+    return false if i < 0 || j < 0 || i >= H || j >= W
+    if input[i][j] == OBSTACLE
+      i -= di
+      j -= dj
+      di, dj = dj, -di
     end
   end
   false
