94a47c8c8594d474cb5f82647b4f03da78d1cfa8
Andriamanitra <mikko77792@gmail.com>
2024-12-07 08:10:17 +0200

    improve aesthetics

--- day07/solution.rb
+++ day07/solution.rb
@@ -1,22 +1,23 @@
-input = ARGF.read.lines(chomp: true).map{|line| line.split(/\D+/).map(&:to_i)}
+input =
+  ARGF
+    .readlines(chomp: true)
+    .map { |line| line.split(/\D+/).map(&:to_i) }
 
 class Integer
   def concat(other) = self * (10 ** other.to_s.size) + other
 end
 
-part1 = input.sum {|target, *nums|
-  [:*, :+].repeated_permutation(nums.size - 1).any?{|ops|
-    ops = ops.each
-    nums.inject{|a, b| a.send(ops.next, b)} == target
-  } ? target : 0
-}
-
-part2 = input.sum {|target, *nums|
-  [:*, :+, :concat].repeated_permutation(nums.size - 1).any?{|ops|
-    o = ops.each
-    nums.inject{|a, b| a > target ? break : a.send(o.next, b)} == target
-  } ? target : 0
-}
+part1, part2 = [%i'* +', %i'* + concat'].map do |operators|
+  input.sum do |target, initial, *nums|
+    operators.repeated_permutation(nums.size).any? do |permutation|
+      ops = permutation.each
+      target == nums.inject(initial) do |acc, n|
+        break if acc > target
+        acc.send(ops.next, n)
+      end
+    end ? target : 0
+  end
+end
 
 puts "Part 1: #{part1}"
 puts "Part 2: #{part2}"
