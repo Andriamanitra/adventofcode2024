f2f5c838251c1b2c62e73ef259c7d532ef175411
Andriamanitra <mikko77792@gmail.com>
2024-12-13 03:01:13 +0200

    formatting & bit of refactoring

--- day12/solution.hs
+++ day12/solution.hs
@@ -5,14 +5,18 @@ import Data.Set (Set)
 import System.IO (readFile')
 
 data Pos = P Int Int deriving (Show, Ord, Eq)
-data Direction = North | East | South | West deriving (Eq, Enum, Bounded, Show)
+data Direction = North | East | South | West deriving (Eq, Ord, Enum, Bounded, Show)
 type Grid a = Map Pos a
 type Region = Set Pos
-data Edge = Edge Pos Direction deriving (Show, Eq)
+data Edge = Edge Pos Direction deriving (Show, Ord, Eq)
 
 makeGrid :: String -> Grid Char
-makeGrid s = Map.fromList [ (P x y, color) | (line, y) <- zip (lines s) [1..]
-                                       , (color, x) <- zip line [1..]]
+makeGrid s = Map.fromList
+  [ (P x y, color)
+  | (line, y) <- zip (lines s) [1..]
+  , (color, x) <- zip line [1..]
+  ]
+
 neighbors :: Pos -> [Pos]
 neighbors pos = [move d pos | d <- [minBound..maxBound]]
 
@@ -30,29 +34,31 @@ regionsOf grid = go (Map.keys grid) mempty mempty
   where
     go :: [Pos] -> Set Pos -> [Region] -> [Region]
     go [] _ g = g
-    go (p:ps) visited regions | Set.member p visited = go ps visited regions
-    go (p:ps) visited regions = go ps (Set.union visited region) (region:regions)
+    go (p:ps) visited regions
+      | Set.member p visited = go ps visited regions
+      | otherwise = go ps (Set.union visited region) (region:regions)
       where region = floodfillFrom p mempty
     floodfillFrom :: Pos -> Set Pos -> Set Pos
     floodfillFrom p region
       | Set.member p region = region
       | otherwise = recur $ Set.insert p region
       where
-        color = grid!?p
-        recur = foldl (.) id $ floodfillFrom <$> filter ((==color).(grid!?)) (neighbors p)
+        sameColor npos = (grid!?p) == (grid!?npos)
+        recur = foldl (.) id $ floodfillFrom <$> filter sameColor (neighbors p)
 
-edgesOf :: Region -> [Edge]
-edgesOf region =
+edgesOf :: Region -> Set Edge
+edgesOf region = Set.fromList
   [ Edge pos direction
   | pos <- Set.elems region
   , direction <- [minBound..maxBound]
   , Set.notMember (move direction pos) region
   ]
 
-sidesOf :: Region -> [Edge]
-sidesOf region =
-  let edges = edgesOf region
-  in [Edge pos d | Edge pos d <- edges, (Edge (move (rot90 d) pos) d) `notElem` edges]
+sidesOf :: Region -> Set Edge
+sidesOf region = Set.filter ((`Set.notMember` edges) . adjacentEdge) edges
+  where
+    edges = edgesOf region
+    adjacentEdge (Edge pos d) = Edge (move (rot90 d) pos) d
 
 fenceCostPart1 :: Region -> Int
 fenceCostPart1 region = length region * length (edgesOf region)
