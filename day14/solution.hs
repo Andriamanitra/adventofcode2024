09e8418b843022f67df0ce41ee17816cf47145db
Andriamanitra <mikko77792@gmail.com>
2024-12-15 04:20:35 +0200

    solve day 14 in Haskell

--- day14/solution.hs
+++ day14/solution.hs
@@ -10,29 +10,55 @@ parseRobot line = Robot pos speed
   where parsePairOfInts s = (read a, read b) where (a, (_:b)) = span (/= ',') s
         [pos, speed] = (parsePairOfInts . drop 2) <$> words line
 
+getPos :: Robot -> Pos
+getPos (Robot pos _) = pos
+
 move :: Int -> Robot -> Robot
 move steps (Robot (x, y) (dx, dy)) = Robot (x + steps * dx, y + steps * dy) (dx, dy)
 
-onGrid :: (Int, Int) -> Robot -> Robot
-onGrid (width, height) (Robot (x, y) speed) = Robot (mod x width, mod y width) speed
+onGrid :: (Int, Int) -> Pos -> Pos
+onGrid (width, height) (x, y) = (mod x width, mod y height)
 
-whichQuadrantOnGrid :: (Int, Int) -> Robot -> Quadrant
-whichQuadrantOnGrid (width, height) (Robot (x, y) _)
-  | xOnGrid < xMid && yOnGrid < yMid = TopLeft
-  | xOnGrid > xMid && yOnGrid < yMid = TopRight
-  | xOnGrid < xMid && yOnGrid > yMid = BottomLeft
-  | xOnGrid > xMid && yOnGrid > yMid = BottomRight
+whichQuadrantOnGrid :: (Int, Int) -> Pos -> Quadrant
+whichQuadrantOnGrid gridSize@(w, h) pos
+  | x < xMid && y < yMid = TopLeft
+  | x > xMid && y < yMid = TopRight
+  | x < xMid && y > yMid = BottomLeft
+  | x > xMid && y > yMid = BottomRight
   | otherwise = None
   where
-    xMid = div width 2
-    yMid = div height 2
-    xOnGrid = mod x width
-    yOnGrid = mod y height
+    (x, y) = onGrid gridSize pos
+    (xMid, yMid) = (div w 2, div h 2)
+
+variance :: [Int] -> Int
+variance xs = sum [(x - mean) ^ 2 | x <- xs]
+  where mean = div (sum xs) (length xs)
+
+xyVariance :: [Pos] -> Int
+xyVariance positions = variance xs * variance ys
+  where (xs, ys) = unzip positions
+
+solvePart1 :: [Robot] -> (Int, Int) -> Int
+solvePart1 robots gridSize = product $ countIn robotQuadrants <$> [TopLeft, TopRight, BottomLeft, BottomRight]
+  where countIn xs x = length $ filter (==x) xs
+        robotQuadrants = whichQuadrantOnGrid gridSize . getPos <$> robots
+
+solvePart2 :: [Robot] -> (Int, Int) -> Int
+solvePart2 robots gridSize@(width, height) = indexOfMinVariance
+  where
+    moveAll = map $ move 1
+    loopLength = width * height
+    getPosOnGrid = onGrid gridSize . getPos
+    xyVariances = xyVariance . map getPosOnGrid <$> iterate moveAll robots
+    (_, indexOfMinVariance) = minimum $ zip xyVariances [0..loopLength]
 
 main = do
   input <- lines <$> readFile' "input.txt"
+  let gridSize = (101, 103)
   let robots = parseRobot <$> input
-  let quadrants = whichQuadrantOnGrid (101, 103) . move 100 <$> robots
-  let countIn xs x = length $ filter (==x) xs
-  let part1 = product $ map (countIn quadrants) [TopLeft, TopRight, BottomLeft, BottomRight]
-  print part1
+
+  let safetyFactor = solvePart1 (move 100 <$> robots) gridSize
+  putStrLn $ "Part 1: " <> show safetyFactor
+
+  let easterEggAppearsAt = solvePart2 robots gridSize
+  putStrLn $ "Part 2: " <> show easterEggAppearsAt
